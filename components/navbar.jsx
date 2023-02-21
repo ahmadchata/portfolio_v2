acf69cac736b8a24474401e87957a81519b5422c
ahmadchata <ahmadchata@gmail.com>
2023-02-21 13:44:23 +0100

    Add Component: Navbar

---
+++ components/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="d-flex justify-content-center pt-3 monteya-font fs-6 shadow-sm sticky-top">
+      <ul className="d-flex list-unstyled">
+        <li className="">HOME</li>
+        <li className="mx-5">PROJECTS</li>
+        <li>BLOG</li>
+        <li className="ms-5">ABOUT</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
+import Navbar from "@/components/navbar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
